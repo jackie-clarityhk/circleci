0208a44ec4f413cfe34d72cb43075f05f0fbfe67
JackieZheng <zhengjiekun@gmail.com>
2014-11-20 18:42:28 +0800

    try

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -16,7 +16,7 @@ if [ ! -e $DEPS ]; then
   
   echo y | android update sdk --no-ui --filter "android-19" &&
   echo y | android update sdk -u -a -t platform-tools &&
-  echo y | android update sdk --no-ui --filter "build-tools-20.0.0" &&
+  echo y | android update sdk --no-ui --filter "build-tools-21.0.0" &&
   echo y | android update sdk --no-ui --filter "tools" &&
   echo y | android update sdk --no-ui --filter "extra-android-m2repository" &&
   echo y | android update sdk --no-ui --filter "extra-android-support" &&
