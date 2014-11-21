85e16e19aa17fda048d490650b46d077b1b913d6
JackieZheng <zhengjiekun@gmail.com>
2014-11-21 10:30:50 +0800

    try

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,14 +14,14 @@ if [ ! -e $DEPS ]; then
   # echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
   # echo n | android create avd -n testing -f -t android-19 &&
   
-  echo y | android update sdk --no-ui --filter "android-19" &&
+  echo y | android update sdk --no-ui --filter "android-20" &&
   echo y | android update sdk -u -a -t platform-tools &&
-  echo y | android update sdk --no-ui --filter "build-tools-23.+" &&
+  echo y | android update sdk --no-ui --filter "build-tools-20.0.0" &&
   echo y | android update sdk --no-ui --filter "tools" &&
   echo y | android update sdk --no-ui --filter "extra-android-m2repository" &&
   echo y | android update sdk --no-ui --filter "extra-android-support" &&
   echo y | android update sdk --no-ui --filter "extra-google-m2repository" &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-19 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-20 &&
    
   touch $DEPS
 fi
